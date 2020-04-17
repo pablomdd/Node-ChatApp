bb4db5a110211258bdfd9fca2a1cbb08732e6e32
pablomdd <pablo.mdd.007@gmail.com>
2020-04-17 01:43:09 -0500

    Add Connection to MongoAtlas and post and get methods support

--- components/message/store.js
+++ components/message/store.js
@@ -1,13 +1,27 @@
+const db = require('mongoose');
+const Model =  require('./model');
 
+// Le decimos a Mongoose que utilice la Clase Promise de Node
+// para cuando vaya a utilizar cualquier promesa. 
+// Esto hace la implementacion de manera nativa
+db.Promise = global.Promise;
+db.connect('mongodb+srv://user:user1234@cluster0-mekh6.mongodb.net/test', {
+    useNewUrlParser: true,
+});
+console.log('[db] Conectado con exito');
 
 const list = [];
 
-function addMessage(mesage){
-    list.push(mesage);
+function addMessage(message){
+    // list.push(mesage);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages(){
-    return list;
+async function getMessages(){
+    // return list;
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
