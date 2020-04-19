d48818c7fd70cdaf2835d0f5c6ea07021f8630e7
pablomdd <pablo.mdd.007@gmail.com>
2020-04-19 00:31:05 -0500

    Add cors and websocket

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./db');
+const app = express();
+const server = require('http').Server(app);
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const socket = require('./socket')
+const db = require('./db');
 const router = require('./network/routes')
 
 db('mongodb+srv://user:user1234@cluster0-mekh6.mongodb.net/test');
 
-var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
-// app.use(router);
 
+socket.connect(server);
 router(app);
 
 app.use('/app', express.static('public'))
 
-app.listen(3000);
-console.log('La app esta escuchandose en http://localhost:3000')
+server.listen(3000, function(){
+    console.log('La app esta escuchandose en http://localhost:3000')
+});
